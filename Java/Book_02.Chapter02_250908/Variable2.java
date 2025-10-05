e146efa5d136c1277f14d089afb4b02c5834bb4b
Hyemi193 <clatks4731@hanmail.net>
2025-10-05 23:09:31 +0900

    Add files via upload

---
+++ Java/Book_02.Chapter02_250908/Variable2.java
@@ -0,0 +1,9 @@
+package Chapter2;
+
+public class Variable2 {
+    public static void main(String[] args) {
+        short sVal = 10;
+        byte bVal = 20;
+        System.out.println(sVal + bVal);
+    }
+}
