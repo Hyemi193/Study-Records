1f1c9f3e1a1bd93008a5cf644d768f39f1360c3d
Hyemi193 <clatks4731@hanmail.net>
2025-10-05 22:16:40 +0900

    Add files via upload

---
+++ Java/Chap_01_250911/_06_Constants.java
@@ -0,0 +1,13 @@
+package Chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82";     // 국가 번호
+        // KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double P1 = 3.141592;     // 원주율
+        final String DATA_OF_BIRTH = "2001-12-31";      // 생년 월일
+
+    }
+}
