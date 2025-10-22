5274e7c7145e85b466b6fee1c56b8db99d85b12e
Hyemi <clatks4731@hanmail.net>
2025-10-22 23:23:59 +0900

    Add files via upload

---
+++ Java/J_Book_16.ArrayList_251020/Subject.java
@@ -0,0 +1,23 @@
+package ArrayList;
+
+public class Subject {
+    // Subject 클래스의 인스턴스 변수
+    private String name;    // 과목 이름
+    private int scorePoint; // 과목 점수
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScorePoint() {
+        return scorePoint;
+    }
+
+    public void setScorePoint(int scorePoint) {
+        this.scorePoint = scorePoint;
+    }
+}
