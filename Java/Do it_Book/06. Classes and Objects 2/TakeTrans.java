bdf8df63c9f89f6e4ab2af6c2042c2a977d5801e
Hyemi <clatks4731@hanmail.net>
2025-12-20 18:06:51 +0900

    Add files via upload

---
+++ Java/Do it_Book/06. Classes and Objects 2/TakeTrans.java
@@ -0,0 +1,22 @@
+// 버스와 지하철 타기
+
+package Cooperation;
+
+public class TakeTrans {
+    public static void main(String[] args) {
+
+        // 학생 두명 생성
+        Student studentJames = new Student("James", 5000);
+        Student studentTomas = new Student("Tomas", 10000);
+
+        Bus bus100 = new Bus(100);      // 노선 번호가 100번인 버스 생성
+        studentJames.takeBus(bus100);             // James가 100번 버스를 탐
+        studentJames.showInfo();                  // James 정보 출력
+        bus100.showInfo();                        // 버스 정보 출력
+
+        Subway subwayblue = new Subway("1호선");  // 노선 번호가 1호선인 지하철 생성
+        studentTomas.takeSubway(subwayblue);               // Tomas가 1호선을 탐
+        studentTomas.showInfo();                           // Tomas 정보 출력
+        subwayblue.showInfo();                             // 지하철 정보 출력
+    }
+}
