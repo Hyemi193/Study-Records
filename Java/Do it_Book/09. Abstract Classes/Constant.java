ac9230749994f447d29cad1b2ceea1998a5a3f60
Hyemi <clatks4731@hanmail.net>
2025-12-20 19:10:10 +0900

    Add files via upload

---
+++ Java/Do it_Book/09. Abstract Classes/Constant.java
@@ -0,0 +1,20 @@
+// 09-4 final 예약어의 쓰임
+// final 변수 이해
+
+package Finalex;
+
+import java.sql.SQLOutput;
+
+public class Constant {
+    int num = 10;
+    final int NUM = 100;    // 상수(변하지 않는 수) 선언
+
+    public static void main(String[] args) {
+        Constant cons = new Constant();
+        cons.num = 50;
+//      cons.NUM = 200;     // 상수에 값을 대입하여 오류 발생
+
+        System.out.println(cons.num);
+        System.out.println(cons.NUM);
+    }
+}
---
+++ Java/Do it_Book/09. Abstract Classes/Define.java
@@ -0,0 +1,13 @@
+// 09-4 final 예약어의 쓰임
+// 여러 파일에서 공유할 수 있는 final 상수 선언
+
+package Finalex;
+
+public class Define {
+    public static final int MIN = 1;
+    public static final int MAX = 99999;
+    public static final int ENG = 1001;
+    public static final int MATH = 2001;
+    public static final double PI = 3.14;
+    public static final String GOOD_MORNING = "Good Morning!";
+}
